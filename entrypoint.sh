df5140fe240799152ec785ab4baf14914fb8fe24
Administrator <gitlab_admin_ac2c7c@example.com>
2026-06-18 12:08:53 +0300

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER"
 do
     sleep 1
 done
-goose -dir migrations postgres \
-"host=$POSTGRES_HOST port=$POSTGRES_PORT user=$POSTGRES_USER password=$POSTGRES_PASSWORD dbname=$POSTGRES_DB sslmode=disable" down-to 0
+# goose -dir migrations postgres \
+# "host=$POSTGRES_HOST port=$POSTGRES_PORT user=$POSTGRES_USER password=$POSTGRES_PASSWORD dbname=$POSTGRES_DB sslmode=disable" down-to 0
 goose -dir migrations postgres \
 "host=$POSTGRES_HOST port=$POSTGRES_PORT user=$POSTGRES_USER password=$POSTGRES_PASSWORD dbname=$POSTGRES_DB sslmode=disable" up
 
